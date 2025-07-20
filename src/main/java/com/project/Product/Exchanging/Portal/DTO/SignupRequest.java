8e4e36e9b1a2f43a26ec9f45825ca81b5afeb949
kalyan <your-email@example.com>
2025-07-20 23:06:57 +0530

    Removed old project files

--- src/main/java/com/project/Product/Exchanging/Portal/DTO/SignupRequest.java
+++
@@ -1,7 +0,0 @@
-package com.project.Product.Exchanging.Portal.DTO;
-
-public class SignupRequest {
-    private String username;
-    private String password;
-    private String email;
-}
--- src/main/java/com/project/Product/Exchanging/Portal/Model/RoleType.java
+++
@@ -1,6 +0,0 @@
-package com.project.Product.Exchanging.Portal.Model;
-
-public enum RoleType {
-    ROLE_USER,
-    ROLE_ADMIN
-}
